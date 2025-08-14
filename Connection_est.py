67dd8ce2b5a4f5b809a0af80f6b9f2da64ee0453
B404KKK-cray <kevin.steven.puika@gmail.com>
2025-08-14 23:34:36 +0700

    Add files via upload

---
+++ Connection_est.py
@@ -0,0 +1,10 @@
+  # my_functions.py
+def greet(name):
+	return f"Hello, {name}!"
+
+def add(a, b):
+	return a + b
+	
+def drug():
+	url = f"https://github.com/B404KKK-cray/ID_Feat_Scoring/raw/refs/heads/main/KKK-jobs.py"
+	return url
